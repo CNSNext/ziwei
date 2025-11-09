89777df9aeb2701538f854881af4e9c3bf68aaa4
ZiMing Liu <lzm0x219@gmail.com>
2025-11-09 15:30:34 +0800

    添加 React 组件文档和示例，更新描述信息，优化代码格式

--- packages/react/playground/App.tsx
+++ packages/react/playground/App.tsx
@@ -4,7 +4,14 @@ import { ZiWei } from "../src";
 export default function App() {
   return (
     <div>
-      <ZiWei width={600} height={600} name="xx" date="1982-1-10-2" gender="male" />
+      <ZiWei
+        width={600}
+        height={600}
+        name="xx"
+        date="1982-1-10-2"
+        gender="male"
+        language="zh-Hant"
+      />
     </div>
   );
 }
